10999a3928561bbf4ad160260a22509f83189dd5
mbz4 <matevz.zorec@gmail.com>
2023-03-16 12:40:02 +0200

    links added to test

--- Practical5/test.py
+++ Practical5/test.py
@@ -1,3 +1,10 @@
+#https://pypi.org/project/pytest/
+# https://python-control.readthedocs.io/en/0.9.3.post2/control.html
+# https://pypi.org/project/control/#history
+# https://colab.research.google.com/github/jckantor/CBE30338/blob/master/docs/05.01-Getting-Started-with-Transfer-Functions.ipynb#scrollTo=xSvoZ1FK9itT
+# https://www.youtube.com/watch?v=NMpmb0ihoFo&list=PLUMWjy5jgHK3-ca6GP6PL0AgcNGHqn33f&index=4
+# https://python-control.readthedocs.io/en/latest/generated/control.sisotool.html
+
 import control
 import matplotlib.pyplot as plt
 import numpy as np
